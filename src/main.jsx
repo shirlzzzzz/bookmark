b0bdfc442e96c2113146bb974b54df0d5b9a054d
shirlzzzzz <shirl.etie@gmail.com>
2026-02-05 14:00:39 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppWrapper.jsx'
+import AppWrapper from './AppWrapper.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
