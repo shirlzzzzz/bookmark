79fda066624e3b4c35a977f62e9fb38b04cd92ef
Reggie E <retie002@gmail.com>
2026-02-20 23:50:29 -0800

    Test: make isbndb catch-all always return JSON

--- api/isbndb/[...path].js
+++ api/isbndb/[...path].js
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store");
   res.status(200).json({
     ok: true,
-    message: "catch-all hit",
+    catchAll: true,
     url: req.url,
-    method: req.method
+    method: req.method,
+    query: req.query
   });
 }
